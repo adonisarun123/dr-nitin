dfe48e4fe8f16fe77c45887393f4cf58fbf45c09
adonisarun123 <adonis.arun@gmail.com>
2025-12-27 22:21:36 +0530

    Replace GTM with Google Analytics gtag.js (G-ECYMSGNRR0)

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,21 +72,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N7HDFR64');`}
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ECYMSGNRR0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ECYMSGNRR0');
+          `}
         </Script>
-        {/* End Google Tag Manager */}
+        {/* End Google tag */}
       </head>
       <body className={cn(inter.variable, poppins.variable, "font-sans bg-background text-text-primary")}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7HDFR64"
-          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
-        {/* End Google Tag Manager (noscript) */}
         <JsonLd data={organizationSchema} />
         <Header />
         {children}
